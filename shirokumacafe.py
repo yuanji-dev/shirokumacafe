e013f06d32f97a911a8dfb38c986844ae52c14ab
Gimo <self@gimo.me>
2018-05-10 22:21:25 +0900

    お待たせしました。

--- shirokumacafe.py
+++ shirokumacafe.py
@@ -1,50 +1,72 @@
 # coding: utf-8
 
+import os
 import requests
+import time
+import glob
+import random
 
+from config import API_KEY, API_SECRET, USERNAME, PASSWORD
 
 API_TOKEN_URL = 'https://frodo.douban.com/service/auth2/token'
 STATUS_URL = 'https://api.douban.com/v2/lifestream/statuses'
 USER_AGENT = 'api-client/2.0 com.douban.shuo/2.2.7(123) Android/22 product/PD1602 vendor/vivo model/vivo X7'
 
-def login(username, password):
+PWD = os.path.dirname(os.path.abspath('__file__'))
+IMAGE_DIR = os.path.join(PWD, 'images')
+TOKEN_FILE = os.path.join(PWD, 'token')
+
+def pick_image():
+    episode_dir = random.choice(glob.glob(os.path.join(IMAGE_DIR, '*')))
+    return random.choice(glob.glob(os.path.join(episode_dir, '*')))
+
+def get_access_token():
+    with open(TOKEN_FILE) as f:
+        return f.read()
+
+def fresh_access_token():
     data = {
-        'client_id': '',
-        'client_secret': '',
+        'client_id': API_KEY,
+        'client_secret': API_SECRET,
         'redirect_uri': '',
         'grant_type': 'password',
-        'username': username,
-        'password': password,
+        'username': USERNAME,
+        'password': PASSWORD,
     }
-
-    res = requests.post(API_TOKEN_URL, data=data)
-    res.raise_for_status()
-    #{'access_token': '', 'douban_user_name': '白熊咖啡馆', 'douban_user_id': '178473869', 'expires_in': 7775999, 'refresh_token': ''}
-    return res.json()
+    r = requests.post(API_TOKEN_URL, data=data)
+    r.raise_for_status()
+    access_token = r.json()['access_token']
+    with open(TOKEN_FILE, 'w') as f:
+        f.write(access_token)
 
 def create_status():
-    with open('./icon.jpg', 'rb') as image:
+    access_token = get_access_token()
+    with open(pick_image(), 'rb') as image:
         files = {
             'image': image
         }
 
         headers = {
             'User-Agent': USER_AGENT,
-            'Authorization': 'Bearer %s',
+            'Authorization': 'Bearer %s' % access_token,
         }
 
         data = {
             'version': 2,
-            'text': 'ようこそ'
+            'text': ''
         }
 
-        res = requests.post(STATUS_URL, headers=headers, files=files, data=data)
-        print(res.content)
-        print(res.json())
+        r = requests.post(STATUS_URL, headers=headers, files=files, data=data)
+        print(r.content)
+        if not r.status_code == requests.codes.ok:
+            fresh_access_token()
+            return False
+        return True
 
 def main():
-    print(login('', ''))
-    create_status()
+    if not create_status():
+        time.sleep(2)
+        create_status()
 
 if __name__ == '__main__':
     main()
